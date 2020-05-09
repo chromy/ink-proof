f121ae6f87a5a9f9d6131ec27a0530f7bb12a2a5
Hector Dearman <hector.dearman@gmail.com>
2020-05-09 22:32:57 +0100

    more

--- test.py
+++ test.py
@@ -1,8 +1,51 @@
+#!/usr/bin/env python3
 import sys
 import os
 import asyncio
 import tempfile
 import contextlib
+import json
+import shutil
+
+
+class Status(object):
+    def __init__(self, name, symbol, description):
+        self.name = name
+        self.symbol = symbol
+        self.description = description
+
+    def describe(self):
+        return {"name": self.name,
+                "description": self.description,
+                "symbol": self.symbol,}
+
+SuccessStatus = Status("SUCCESS", "💚", "")
+ErrorStatus = Status("ERROR", "❌", "")
+CrashedStatus = Status("CRASHED", "🔥", "")
+TimeoutStatus = Status("TIMEOUT", "⌛", "")
+
+class PlayerResult(object):
+    def __init__(self, program, example, player_job, diff_job):
+        self.program = program
+        self.example = example
+        self.player_job = player_job
+        self.diff_job = diff_job
+
+    def settle(self):
+        if self.player_job.return_code != 0:
+            self.status = CrashedStatus
+        elif self.diff_job.return_code == 1:
+            self.status = ErrorStatus
+        else:
+            self.status = SuccessStatus
+
+    def describe(self):
+        return {
+            "status": self.status.name,
+            "player": self.program.name,
+            "example": self.example.name,
+            "diff": self.diff_job.stdout_path,
+        }
 
 class BytecodeExample(object):
     def __init__(self, name, bytecode_path, input_path, transcript_path):
@@ -10,6 +53,17 @@ class BytecodeExample(object):
         self.bytecode_path = bytecode_path
         self.input_path = input_path
         self.transcript_path = transcript_path
+
+    def __lt__(self, o):
+        return self.name < o.name
+
+    def describe(self):
+        return {
+            "name": self.name,
+            "sourcePath": self.bytecode_path,
+            "inputPath": self.input_path,
+            "expectedOutputPath": self.transcript_path,
+        }
 
     @staticmethod
     def fromDirAndName(root, name):
@@ -24,6 +78,9 @@ class InkExample(object):
         self.input_path = input_path
         self.transcript_path = transcript_path
 
+    def __lt__(self, o):
+        return self.name < o.name
+
     @staticmethod
     def fromDirAndName(root, name):
         ink_path = os.path.join(root, name + '.ink')
@@ -36,26 +93,37 @@ class PlayerDriver(object):
         self.name = name
         self.path = path
 
+    def __lt__(self, o):
+        return self.name < o.name
+
+    def describe(self):
+        return {
+            "name": self.name,
+        }
+
 def find_all_bytecode_examples(root):
     folder = os.path.join(root, 'bytecode')
     files = os.listdir(folder)
     names = set([name.split('.')[0] for name in files])
     examples = [BytecodeExample.fromDirAndName(folder, name) for name in names]
-    return examples
+    return sorted(examples)
 
 def find_all_ink_examples(root):
     folder = os.path.join(root, 'ink')
     files = os.listdir(folder)
     names = set([name.split('.')[0] for name in files])
     examples = [InkExample.fromDirAndName(folder, name) for name in names]
-    return examples
+    return sorted(examples)
 
 def find_all_player_drivers(root):
-    return [PlayerDriver('inkjs', os.path.join(root, 'player_drivers', 'inkjs', 'player'))]
+    folder = os.path.join(root, 'players')
+    return sorted([
+        PlayerDriver('inkjs', os.path.join(root, 'player_drivers', 'inkjs', 'player')),
+        PlayerDriver('test', os.path.join(root, 'player_drivers', 'test', 'player')),
+        ])
 
 def find_all_complier_drivers(root):
     return []
-
 
 class Job(object):
     def __init__(self, command, stdout_path=None, stderr_path=None, stdin_path=None, deps=None):
@@ -65,6 +133,7 @@ class Job(object):
         self.stdout_path = stdout_path
         self.task = None
         self.deps = deps if deps else []
+        self.return_code = None
 
     def begin(self):
         self.task = asyncio.create_task(self.run())
@@ -72,12 +141,15 @@ class Job(object):
     async def run(self):
         if self.deps:
             done, pending = await asyncio.wait([dep.task for dep in self.deps])
+        for dep in self.deps:
+            if dep.return_code != 0:
+                return
         fin = open(self.stdin_path) if self.stdin_path else None
         fout = open(self.stdout_path, 'wb') if self.stdout_path else None
         ferr = open(self.stderr_path, 'wb') if self.stderr_path else None
         print('Running "{}"'.format(' '.join(self.command)))
         process = await asyncio.create_subprocess_exec(self.command[0], *self.command[1:], stdout=fout, stderr=ferr, stdin=fin)
-        returncode = await process.wait()
+        self.return_code = await process.wait()
         if fout:
             fout.close()
         if ferr:
@@ -118,7 +190,21 @@ def ensure_dir(directory):
         os.makedirs(directory)
     return directory
 
-def render_results(fout):
+def write_json(fout, programs, examples, results):
+    metadata = {}
+    statuses = {status.name: status.describe() for status in [
+        ErrorStatus, SuccessStatus, TimeoutStatus, CrashedStatus]}
+    programs = [program.describe() for program in programs]
+    examples = [example.describe() for example in examples]
+    results = [result.describe() for result in results]
+
+    json.dump({
+        "metadata": metadata,
+        "statuses": statuses,
+        "programs": programs,
+        "examples": examples,
+        "results": results,
+    }, fout)
 
     # For each example
     # For each runner
@@ -129,11 +215,6 @@ def render_results(fout):
     # - Player output non-determnistic
     # - Player output diff from transcript
     # - Correct
-    fout.write(b'<html>\n')
-    fout.write(b'<body>\n')
-    fout.write(b'<h1>Hello!</h1>\n')
-    fout.write(b'</body>\n')
-    fout.write(b'</html>\n')
     # statues
     # examples {name, description, inputs, expected output}
     # programs {name, etc}
@@ -149,18 +230,29 @@ def main(root):
         output_directory = ensure_dir('out')
 
         jobs = []
-        for player in player_drivers:
-            for example in bytecode_examples:
-                j = player_job(player, example, output_directory)
-
+        results = []
+        for i, player in enumerate(player_drivers):
+            for j, example in enumerate(bytecode_examples):
+                job_a = player_job(player, example, output_directory)
                 diff_path = os.path.join(output_directory, name(player, example, suffix='_diff.txt'))
-                d = diff_job(example.transcript_path, j.stdout_path, diff_path, deps=[j])
-                jobs.append(j)
-                jobs.append(d)
+                job_b = diff_job(example.transcript_path, job_a.stdout_path, diff_path, deps=[job_a])
+                jobs.append(job_a)
+                jobs.append(job_b)
+                results.append(PlayerResult(player, example, job_a, job_b))
         asyncio.run(run_jobs(jobs))
 
-        fout = context_stack.enter_context(open(os.path.join(output_directory, 'index.html'), 'wb'))
-        render_results(fout)
+        for result in results:
+            result.settle()
+        fout = context_stack.enter_context(open(os.path.join(output_directory, 'summary.json'), 'w'))
+
+        write_json(fout, player_drivers, bytecode_examples, results)
+        shutil.copyfile(os.path.join(root, 'index.html'), os.path.join(output_directory, 'index.html'))
+
+        output_bytecode_path = os.path.join(output_directory, 'bytecode')
+        if os.path.exists(output_bytecode_path):
+            shutil.rmtree(os.path.join(output_directory, 'bytecode'))
+        shutil.copytree(os.path.join(root, 'bytecode'), output_bytecode_path)
+
 
 
 if __name__ == '__main__':
