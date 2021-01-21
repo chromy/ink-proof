a45104900326e438c1c1ef577b6ea8c26afdf60d
Hector Dearman <hector.dearman@gmail.com>
2021-01-21 02:05:17 +0000

    Ignore line endings when diffing

--- diff.py
+++ diff.py
@@ -9,11 +9,11 @@ if len(sys.argv) != 3:
 
 _, a, b = sys.argv
 
-a_lines = codecs.open(a, encoding='utf-8-sig').read().splitlines(keepends=True)
-b_lines = codecs.open(b, encoding='utf-8-sig').read().splitlines(keepends=True)
+a_lines = codecs.open(a, encoding='utf-8-sig').read().splitlines()
+b_lines = codecs.open(b, encoding='utf-8-sig').read().splitlines()
 
 
-out_lines = list(unified_diff(a_lines, b_lines, fromfile=a, tofile=b))
+out_lines = list(unified_diff(a_lines, b_lines, fromfile=a, tofile=b, lineterm=""))
 
 if out_lines:
   sys.stdout.writelines(out_lines)
